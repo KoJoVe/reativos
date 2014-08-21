d9e729bc99979484709b33b1840a408104187f23
KoJoVe <joao_kubo_@hotmail.com>
2014-08-21 18:55:28 -0300

    Finaliza e comenta

--- arduino.c
+++ arduino.c
@@ -1,21 +1,21 @@
-//Código que permite controlar a velocidade do pisca pisca de um LED utilizando um botão
-//para incremento e outro botão para decremento da velocidade. Ao pressionar os dois botões
-//simuntaneamente (entre um intervalo preestabelecido), o programa para.
-
-//AINDA NÃO TESTADO NO ARDUINO 
+//Tarefa 2 de sistemas reativos
+//Programa que controla a velocidade do pisca pisca de um LED atraves de dois botoes.
+//Um botao serve para aumentar a frequencia com que a luz pisca e outro serve para diminuir.
+//O botao so faz efeito a cada pressionada. Segurar o botao so ira modificar o tempo uma vez.
+//Apertar os dois botoes simuntaneamente (entre um intervalo predefinido), congela o programa.
 
 #define LED_PIN 13
 #define BUT1_PIN 3
 #define BUT2_PIN 4
-#define PRESS_INTERVAL 50 //Intervalo para pressionar os dois botões.
-#define INTERVAL_INCREMENT 20
+#define PRESS_INTERVAL 50 //Intervalo de tempo entre a ativacao dos dois botoes para desligar o programa.
+#define INTERVAL_INCREMENT 20 //Quantidade de tempo a ser modificada ao pressionar de um botao.
 
-int interval = 100;
+int interval = 100; //Intervalo (milisegundos) para o LED mudar seu estado.
 
-int state = 1;
+int state = 1; //Estado inicial do LED (1 para ligado, 0 para desligado).
 unsigned long old;
 unsigned long pressTime;
-bool b1p = false;
+bool b1p = false; //Variaveis para identificacao de quais botoes estao sendo pressionados.
 bool b2p = false;
 
 void setup() {
@@ -25,7 +25,6 @@ void setup() {
   pinMode(BUT1_PIN, INPUT);
   pinMode(BUT2_PIN, INPUT);
   digitalWrite(LED_PIN, state);
-  Serial.begin(9600);
 
 }
 
@@ -35,21 +34,19 @@ void loop() {
   int but1 = digitalRead(BUT1_PIN);
   int but2 = digitalRead(BUT2_PIN);
   
-  if(now >= old+interval) {
+  if(now >= old+interval) { //Realiza a modificacao do estado do LED de acordo com o intervalo atual.
     old = now;
     state = !state;
     digitalWrite(LED_PIN, state);
-    Serial.println(state);
-    Serial.println(interval);
   }
 
-  if(but1) {
-    if(!b1p) {
-      interval += INTERVAL_INCREMENT;
+  if(but1) { //Botao 1
+    if(!b1p) { //Verifica se o botao ja esta pressionado.
+        interval += INTERVAL_INCREMENT;
       b1p = true;
-      if(!but2)  
-        pressTime = now;
-    } else {
+      if(!but2) //Verifica se o outro botao ja esta pressionado.
+        pressTime = now; //Registra o momento em que o botao foi pressionado.
+    } else { //Caso o botao esteja pressionado, ao pressionar o outro dentro do intervalo de tempo definido o programa para.
       if(now <= pressTime+PRESS_INTERVAL&&but2) 
         while(1);
     }
@@ -59,7 +56,8 @@ void loop() {
 
   if(but2) {
     if(!b2p) {
-      interval -= INTERVAL_INCREMENT;
+      if(interval>INTERVAL_INCREMENT*2)
+        interval -= INTERVAL_INCREMENT;
       b2p = true;
       if(!but1)
         pressTime = now;
