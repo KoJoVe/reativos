086f8a7126b362905cdeb2f57168450959281c64
jnassar <joao_kubo_@hotmail.com>
2014-08-28 17:01:49 -0300

    Adiciona oneButton.c
    Programa que usa a biblioteca event driven para fazer o led seguir o
    estado de um botao escolhido

---
+++ Tarefa 3/oneButton.c
@@ -0,0 +1,13 @@
+#include “event_driven.c”
+
+#define LED_PIN 13
+#define BUT_PIN  2
+
+void button_changed (int pin, int v) {
+    digitalWrite(LED_PIN, v);
+}
+
+void initialize () {
+	pinMode(LED_PIN, OUTPUT);
+    button_listen(BUT_PIN);
+}
