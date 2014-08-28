9200a4bdc7f5b09f80f7653f6eeab72b3dd79a68
jnassar <joao_kubo_@hotmail.com>
2014-08-28 17:02:57 -0300

    Corrige as chamadas das funcoes

--- Tarefa 3/oneButton.c
+++ Tarefa 3/oneButton.c
@@ -3,11 +3,11 @@
 #define LED_PIN 13
 #define BUT_PIN  2
 
-void button_changed (int pin, int v) {
+void button1_changed (int pin, int v) {
     digitalWrite(LED_PIN, v);
 }
 
 void initialize () {
 	pinMode(LED_PIN, OUTPUT);
-    button_listen(BUT_PIN);
+    button1_listen(BUT_PIN);
 }
